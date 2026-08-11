614249c98d36d70bcaae513e3645f337561295d3
palani1803 <ppalani1803@gmail.com>
2026-08-11 15:59:15 +0530

    merge

--- src/pages/ChitScheme.tsx
+++ src/pages/ChitScheme.tsx
@@ -520,7 +520,8 @@ const ChitScheme: React.FC = () => {
                         </div>
                       ) : null}
                     </div>
-                  ))}
+                  </div>
+                ))}
               </div>
             )}
           </div>
