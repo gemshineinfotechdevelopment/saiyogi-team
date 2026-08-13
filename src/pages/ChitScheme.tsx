286263d0c9315a461be97b8d72e226c91dfa06d7
hari <hariharasudhanr735@gmail.com>
2026-08-13 11:25:56 +0530

    Update ChitScheme header comment

--- src/pages/ChitScheme.tsx
+++ src/pages/ChitScheme.tsx
@@ -1,3 +1,4 @@
+// Sai Yogi Chit Scheme Component
 import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import UserHeader from "@/components/layout/UserHeader";
